0a47a9ab46acc247078826608ffa1dd9bfe8c695
Bertrand31 <bertrandjun@gmail.com>
2020-04-03 14:22:31 +0200

    Add source and destination bridges

--- src/main/scala/airportmatching/Main.scala
+++ src/main/scala/airportmatching/Main.scala
@@ -1,16 +1,51 @@
 package airportmatching
 
+import scala.util.Try
+import scala.collection.mutable.Queue
+import cats.implicits._
 import utils.FileUtils
 
 object Main extends App {
 
+  val destinationBridge = new DestinationBridge[String]
+
   DataLoader.hydrateKDTree.foreach(tree => {
-    FileUtils.readFile("src/main/resources/data/user-geo-sample.csv").foreach(iterator => {
-      iterator.drop(1).foreach(user => {
-        val Array(uid, lat, long) = user split ','
-        val nearest = tree.nearest((lat.toFloat, long.toFloat)).value
-        println(s"uid: $uid, nearest: $nearest")
+    SourceBridge.read("user-geo-sample.csv").foreach(iterator => {
+      iterator.foreach(user => {
+        val (uid, coordinates) = user
+        val nearest = tree.nearest(coordinates).value
+        destinationBridge.write(s"uid: $uid, nearest: $nearest")
       })
     })
   })
+}
+
+object SourceBridge {
+
+  def read(filename: String): Try[Iterator[(String, (Float, Float))]] =
+    FileUtils
+      .readFile("src/main/resources/data/" |+| filename)
+      .map(
+        _
+          .drop(1)
+          .map(_ split ',')
+          .map({
+            case Array(uid, lat, long) => (uid, (lat.toFloat, long.toFloat))
+          })
+      )
+}
+
+class DestinationBridge[A](private val batch: Queue[A] = Queue.empty[A]) {
+
+  private object FakeDB {
+    def write(a: IterableOnce[A]): Unit = println(Iterator.from(1).zip(a).mkString(": "))
+  }
+
+  def write(item: A, batchSize: Int = 1000): Unit = {
+    batch.enqueue(item)
+    if (this.batch.size >= batchSize) {
+      FakeDB.write(batch.iterator)
+      batch.clear
+    }
+  }
 }
