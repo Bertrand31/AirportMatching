6e3b11f76c8ed8ce115cc7bca9e431e7b3d24bdd
Bertrand31 <bertrandjun@gmail.com>
2020-04-03 13:35:33 +0200

    Rename files apropriately

--- src/main/scala/airportmatching/Main.scala
+++ src/main/scala/airportmatching/Main.scala
@@ -7,7 +7,7 @@ object Main extends App {
   DataLoader.hydrateKDTree.foreach(tree => {
     FileUtils.readFile("src/main/resources/data/user-geo-sample.csv").foreach(iterator => {
       iterator.drop(1).foreach(user => {
-        val Array(uid, lat, long) = user.split(",")
+        val Array(uid, lat, long) = user split ','
         val nearest = tree.nearest((lat.toFloat, long.toFloat)).value
         println(s"uid: $uid, nearest: $nearest")
       })
