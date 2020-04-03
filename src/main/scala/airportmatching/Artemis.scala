770a937b5179c029cc253f4d123d4abadd1cb9a2
Bertrand31 <bertrandjun@gmail.com>
2020-04-03 15:04:54 +0200

    Clean up Artemis, improve code style

--- src/main/scala/airportmatching/Artemis.scala
+++ src/main/scala/airportmatching/Artemis.scala
@@ -2,7 +2,7 @@ package airportmatching
 
 import cats.implicits._
 
-// Based on https://rosettacode.org/wiki/K-d_tree, and heavily modified for my specific use-case
+// Based on https://rosettacode.org/wiki/K-d_tree, and heavily modified for this specific use-case
 object Artemis {
 
   def apply(points: Seq[Airport], depth: Int = 0): Option[ArtemisNode] =
@@ -11,8 +11,7 @@ object Artemis {
       val axis = depth % 2
       def getWAxis(t: Point): Float = if (axis === 0) t._1 else t._2
       val sorted = points.sortBy(t => getWAxis(t._1))
-      val medianTuple = sorted(sorted.size / 2)._1
-      val median = getWAxis(medianTuple)
+      val median = getWAxis(sorted(sorted.size / 2)._1)
       val (left, right) = sorted.partition(v => getWAxis(v._1) < median)
       Some(ArtemisNode(right.head, apply(left, depth + 1), apply(right.tail, depth + 1), axis))
     }
@@ -21,12 +20,11 @@ object Artemis {
   def distsq(a: Point, b: Point): Double =
     Math.pow(a._1 - b._1, 2) + Math.pow(a._2 - b._2, 2)
 
-  def compare(a: Point, b: Point): Int = {
-    val (xDiff, yDiff) = (a._1 compare b._1, a._2 compare b._2)
-    if (xDiff =!= 0) xDiff
-    else if (yDiff =!= 0) yDiff
-    else 0
-  }
+  def compare(a: Point, b: Point): Int =
+    (a._1 compare b._1, a._2 compare b._2) match {
+      case (0, 0) => 0
+      case (xDiff, yDiff) => if (xDiff =!= 0) xDiff else yDiff
+    }
 }
 
 final case class ArtemisNode(
@@ -52,4 +50,3 @@ final case class ArtemisNode(
 final case class Nearest(value: Airport, private val to: Point) {
   lazy val distsq = Artemis.distsq(value._1, to)
 }
-
