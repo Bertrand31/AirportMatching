9ad7d82371268643c01adb97f284873e678dc70b
Bertrand31 <bertrandjun@gmail.com>
2020-04-06 14:42:18 +0200

    Improve main method to build Artemis and read CSV in parallel

--- src/main/scala/airportmatching/Main.scala
+++ src/main/scala/airportmatching/Main.scala
@@ -8,16 +8,19 @@ object Main extends IOApp {
   private val destinationBridge = new LogDestinationBridge[String]
 
   def run(args: List[String]): IO[ExitCode] =
-    DataLoader.hydrateArtemis
-      .flatMap(artemis =>
-        CSVSourceBridge.read("user-geo-sample.csv").flatMap(
-          _
-            .map(user => {
-              val nearest = artemis.nearest(user.location)
-              destinationBridge.write(s"user: $user | airport: $nearest")
-            })
-            .toList
-            .sequence
-        )
-      ).as(ExitCode.Success)
+    (
+      DataLoader.hydrateArtemis,
+      CSVSourceBridge.read("user-geo-sample.csv"),
+    )
+      .parMapN((artemis, users) =>
+        users
+          .map(user => {
+            val nearest = artemis.nearest(user.location)
+            destinationBridge.write(s"user: $user | airport: $nearest")
+          })
+          .toList
+          .sequence
+      )
+      .flatten
+      .as(ExitCode.Success)
 }
